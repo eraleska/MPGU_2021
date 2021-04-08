9e79e91a79b33ac6e2dc3b5a4f9fe17145525ed4
eraleska <garettool@mail.ru>
2021-04-08 20:08:21 +0300

    Update NLogNSort.java

--- NLogNSortTest/src/NLogNSort.java
+++ NLogNSortTest/src/NLogNSort.java
@@ -1,8 +1,60 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class NLogNSort {
-    public void sort(int[] data)
-    {
-        Arrays.sort(data); // Replace this
+
+  private static int[] a;
+    private static int n;
+    private static int left;
+    private static int right;
+    private static int largest;
+    public static void buildheap(int []a) {
+        n = a.length-1;
+        for(int i=n/2; i>=0; i--){
+            maxheap(a,i);
+        }
+    }
+
+    public static void maxheap(int[] a, int i) {
+        left = 2*i;
+        right = 2*i+1;
+
+        if(left <= n && a[left] > a[i]){
+            largest=left;
+        } else {
+            largest=i;
+        }
+
+        if(right <= n && a[right] > a[largest]) {
+            largest=right;
+        }
+        if(largest!=i) {
+            exchange(i, largest);
+            maxheap(a, largest);
+        }
+    }
+
+    public static void exchange(int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void sort(int[] myarray) {
+        a = myarray;
+        buildheap(a);
+        for(int i=n; i>0; i--) {
+            exchange(0, i);
+            n=n-1;
+            maxheap(a, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int []numbers={87,34,50,4,42,18,91,5,14,36,27};
+        System.out.println(Arrays.toString(numbers));
+        sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
+
